49a738eecb9faa6f97ef998335f736d7f63a4e4d
sanyashetty <shettysanya@gmail.com>
2023-07-18 13:57:30 -0400

    added defib model

--- backend/defib_radar/defib_radar_app/models.py
+++ backend/defib_radar/defib_radar_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+
+
+class Defibrillator(models.Model):
+    title = models.CharField(max_length=120)
+    address = models.TextField()
+    coordinates = ArrayField(models.FloatField(), size=2)
+    available = models.BooleanField(default=False)
+    # access_type public = true, non-public = false
+    access_type = models.BooleanField(default=False)
+    access_times = models.TextField()
+
+    def _str_(self):
+        return self.title
