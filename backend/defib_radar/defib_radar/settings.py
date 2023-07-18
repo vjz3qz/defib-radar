d33267c7edb095b6d534c8105ce6e1d4033ed4b6
sanyashetty <shettysanya@gmail.com>
2023-07-18 13:04:05 -0400

    added middleware and cors_origin_whitelist

--- backend/defib_radar/defib_radar/settings.py
+++ backend/defib_radar/defib_radar/settings.py
@@ -55,6 +55,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = "defib_radar.urls"
@@ -140,3 +142,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000'
+]
