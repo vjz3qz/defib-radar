af82da88a50939369752dec3ed8f7037990f0aab
sanyashetty <shettysanya@gmail.com>
2023-07-31 17:47:23 -0400

    changed model

--- backend/defib_radar/defib_radar_app/models.py
+++ backend/defib_radar/defib_radar_app/models.py
@@ -5,19 +5,15 @@ from django.contrib.gis.db import models
 
 
 class Defibrillator(models.Model):
-    title = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
-    coordinates = models.PointField(srid=4326, null=True)
-    availability = models.BooleanField(default=False)
-    # access_type public = true, non-public = false
-    access_type = models.BooleanField(default=False)
-    # blank means 24/7, null means 0/0 or unknown
-    #     access_times=json.dumps({
-    #     'monday': {'open': '09:00', 'close': '17:00'},
-    #     'tuesday': {'open': '09:30', 'close': '18:00'},
-    #     # ... and so on for other days
-    #     })
-    access_times = models.JSONField(blank=True, null=True)
+    name = models.CharField(max_length=255)
+    address_line1 = models.CharField(max_length=255)
+    address_line2 = models.CharField(max_length=255, null=True, blank=True, default=None)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    zipcode = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    
 
     def _str_(self):
         return self.title
--- backend/defib_radar/defib_radar_app/views.py
+++ backend/defib_radar/defib_radar_app/views.py
@@ -2,9 +2,34 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import DefibrillatorSerializer
 from .models import Defibrillator
+import requests
 
 # Create your views here.
 
 class DefibrillatorView(viewsets.ModelViewSet):
     serializer_class = DefibrillatorSerializer
     queryset = Defibrillator.objects.all()
+
+# def get_defibs(request):
+#     all_defibs = {}
+#     if 'name' in request.GET:
+#         name = request.GET['name']
+#         url = 'https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Health_WebMercator/MapServer/9/query?where=1%3D1&outFields=*&outSR=4326&f=json' % name
+#         response = requests.get(url)
+#         data = response.json()
+#         defibs = data['defibs']
+
+#         for i in defibs:
+#             defib_data = Defibrillator(
+#                 title = i['LOCATIONNAME'],
+#                 address_line1 = i['ADDRESSLINE1'],
+#                 coordinates = i['strInstructions'],
+#                 availability = i['strArea'],
+#                 access_type = i['idMeal'],
+#                 access_times = i['strMealThumb']
+#             )
+#             defib_data.save()
+#             all_meals = Meal.objects.all().order_by('-id')
+
+#     return render (request, 'meals/meal.html', { "all_meals": 
+#     all_meals} )
